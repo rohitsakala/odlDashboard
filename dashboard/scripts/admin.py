86c476737a4e01296431fa494d600f6997addcd2
Rohit <rohitsakala@gmail.com>
2016-08-23 23:18:11 +0530

    Added per_results to admin page

--- dashboard/scripts/admin.py
+++ dashboard/scripts/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Projects,Bugs,Urls,PerformanceGraphs,robot_results
+from .models import Projects,Bugs,Urls,PerformanceGraphs,robot_results,perf_results
 
 admin.site.register(Projects)
 admin.site.register(Bugs)
 admin.site.register(Urls)
 admin.site.register(PerformanceGraphs)
 admin.site.register(robot_results)
+admin.site.register(perf_results)
 
